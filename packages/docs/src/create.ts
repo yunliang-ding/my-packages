0e4b40f18848376d95eaf9f25b4bb35f8f936187
yunliang <804408086@qq.com>
2024-09-13 17:51:50 +0800

    fixed

--- packages/docs/src/create.ts
+++ packages/docs/src/create.ts
@@ -357,7 +357,7 @@ export const createIndexHtml = async function (
           if (message.data === "1") {
             location.reload();
           } else {
-            document.querySelector("#lyr-cli-client-overlay")?.remove();
+            document.querySelector("#lyr-docs-client-overlay")?.remove();
             const iframe = document.createElement('iframe');
             iframe.id = "lyr-docs-client-overlay"
             iframe.style = "padding: 20px;background: #222;opacity: .9;position: fixed; inset: 0px; width: 100vw; height: 100vh; border: none; z-index: 2147483647;"
@@ -365,7 +365,7 @@ export const createIndexHtml = async function (
             document.querySelector("#lyr-docs-client-overlay").contentWindow.document.body.innerHTML = \`
                     <div style="font-size: 20px;color: red;margin-bottom: 20px;">编译异常</div>
                     <div id="close" style="cursor: pointer; position: fixed; top: 0; right: 40px;"><svg viewBox="0 0 1024 1024" width="18" height="18"><path d="M512 421.490332 331.092592 240.582924C306.351217 215.841549 265.464551 215.477441 240.470996 240.470996 215.303191 265.638801 215.527553 306.037221 240.582924 331.092592L421.490332 512 240.582925 692.907407C215.84155 717.648782 215.477441 758.535449 240.470996 783.529004 265.638801 808.696809 306.037222 808.472446 331.092593 783.417075L512 602.509668 692.907407 783.417075C717.648782 808.15845 758.535449 808.522559 783.529004 783.529004 808.696809 758.361199 808.472446 717.962778 783.417075 692.907407L602.509668 512 783.417076 331.092592C808.158451 306.351217 808.522559 265.464551 783.529004 240.470996 758.361199 215.303191 717.962779 215.527553 692.907408 240.582924L512 421.490332Z" fill="#fff"></path></svg></div>
-                    <pre style="color: red; margin: 10px 0">$\{message.data\}</pre>
+                    <pre style="color: red; margin: 10px 0;white-space: pre-wrap;">$\{message.data\}</pre>
                   \`;
             document.querySelector("#lyr-docs-client-overlay").contentWindow.document.querySelector("#close").addEventListener('click', () => {
               document.querySelector("#lyr-docs-client-overlay").remove()
