6194f8d85798be52af75d72bbfb6cc4b25305e2d
yunliang <804408086@qq.com>
2024-09-18 14:42:11 +0800

    clean

--- lyr.config.ts
+++ lyr.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   favicon:
     'https://lyr-cli-oss.oss-cn-beijing.aliyuncs.com/assets/user-logo.png',
   monorepo: true,
-  // monorepoPackages: ['@yl-d/design'],
+  monorepoPackages: ['@yl-d/design'],
   link: [
     'https://lyr-cli-oss.oss-cn-beijing.aliyuncs.com/cdn/monaco-file-icon.css',
     'https://lyr-cli-oss.oss-cn-beijing.aliyuncs.com/cdn/@yl-d/design.min.css',
